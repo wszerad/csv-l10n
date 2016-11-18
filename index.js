16e3ec6ed5b39a783ac2bf86314ece40c725182f
Wszerad <wszerad91@gmail.com>
2016-11-18 14:56:17 +0100

    v2.0

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var fs = require('fs'),
+	_ = require('lodash'),
 	parse = require('csv-parse/lib/sync');
 
 function L10n(path, opt, cb) {
-	if(typeof opt === 'function') {
+	if (typeof opt === 'function') {
 		cb = opt;
 		opt = {};
 	}
@@ -13,123 +14,129 @@ function L10n(path, opt, cb) {
 	this.path = path;
 	this.blocks = {
 		blockStart: opt.blockStart || '__(',
-		blockEnd: opt.blockEnd || ')',
-		templateStart:  opt.templateStart || '${',
-		templateEnd: opt.templateEnd || '}',
-		bindingStart: opt.bindingStart || '{{',
-		bindingEnd: opt.bindingEnd || '}}'
+		blockEnd: opt.blockEnd || ')'
 	};
 	this.__blockRegExp = new RegExp('(' + regExpEscape(this.blocks.blockStart) + '.+?' + regExpEscape(this.blocks.blockEnd) + ')', 'g');
-	this.__templateRegExp = new RegExp('(' + regExpEscape(this.blocks.templateStart) + '\\d+' + regExpEscape(this.blocks.templateEnd) + ')', 'g');
 	this.errors = [];
 	this.langs = [];
 	this.count = 0;
 	this.lang = null;
 	this.count = 0;
 
-	if(cb) {
+	if (cb) {
 		this.reload(cb);
 	} else {
 		this.reloadSync();
 	}
 }
 L10n.prototype = {
-	render: function(str, lang) {
+	render: function (str, lang) {
 		this.errors.length = 0;
 		this.count = 0;
 
-		if(lang) {
+		if (lang) {
 			this.lang = lang;
-		} else if(this.lang === null && !this.langs.length) {
+		} else if (this.lang === null && !this.langs.length) {
 			throw new Error('No language file detected');
-		} else if(this.lang === null) {
+		} else if (this.lang === null) {
 			this.lang = this.langs[0];
 		}
 
 		return str.replace(this.__blockRegExp, this.injector.bind(this));
 	},
 
-	setLang: function(lang) {
-		if(this.langs.indexOf(lang) !== -1) {
+	setLang: function (lang) {
+		if (this.langs.indexOf(lang) !== -1) {
 			this.lang = lang;
 		} else {
-			throw new Error('Non-existent language: '+lang);
+			throw new Error('Non-existing language: ' + lang);
 		}
 
 		return this;
 	},
 
-	reload: function(cb) {
+	reload: function (cb) {
 		var self = this;
 
-		fs.readFile(self.path, function(err, data) {
-			if(err)
-				return cb(err);
+		fs.readFile(self.path, function (err, data) {
+			if (err) return cb(err);
 
-			self.reloadSync(parse(data));
-
-			cb(null);
+			try {
+				self.reloadSync(parse(data));
+				cb(null);
+			} catch (e) {
+				cb(e);
+			}
 		});
 	},
 
-	reloadSync: function(csv) {
+	reloadSync: function (csv) {
+		function patcher(originPath, path, value, parent, index) {
+			if (path.length === index + 1) {
+				if (parent[path[index]]) {
+					throw new Error('Can not overwrite existing tree object with value! key(' + originPath + ')');
+				}
+
+				parent[path[index]] = value;
+				return;
+			}
+
+			if (parent[path[index]] === undefined) {
+				parent = parent[path[index]] = {};
+			} else {
+				if (!_.isPlainObject(parent[path[index]])) {
+					throw new Error('Can not overwrite existing value with tree branch! key(' + originPath + ')');
+				}
+
+				parent = parent[path[index]];
+			}
+
+			patcher(originPath, path, value, parent, ++index)
+		}
+
 		var self = this,
 			data = csv || parse(fs.readFileSync(self.path));
 
-		var column = data[0],
-			langs = [],
-			ret = {};
+		self.langs = [];
+		self.resources = {};
 
-		for(var i=1; i<column.length; i++) {
-			langs.push(column[i]);
-		}
-
-		self.langs = langs;
-		self.resources = langs.reduce(function(ret, lang, index) {
-			var clang = ret[lang] = {},
-				position = index+1;
-
-			for(var j=1; j<data.length; j++) {
-				clang[data[j][0]] = self.parseTemplate(data[j][position]);
-			}
-
-			return ret;
-		}, ret);
-	},
-
-	parseTemplate: function(str) {
-		var self = this,
-			ret = {};
-
-		ret.parts = str.split(self.__templateRegExp);
-		ret.parts.forEach(function(part, index) {
-			if(self.__templateRegExp.test(part)) {
-				var num = part.slice(self.blocks.templateStart.length, self.blocks.templateEnd.length*-1);
-				ret[num] = index;
-			}
+		data[0].slice(1).forEach(function (lang) {
+			self.langs.push(lang);
+			self.resources[lang] = [];
 		});
 
-		return ret;
+		self.langs.forEach(function (lang, langIndex) {
+			data.slice(1).forEach(function (res) {
+				var path = res[0].trim();
+
+				patcher(path, _.toPath(path), res[langIndex + 1], self.resources[lang], 0);
+			});
+		});
 	},
 
-	injector: function(str) {
+	injector: function (str) {
+		function prop(lang, path) {
+			if (path.endsWith('.*')) {
+				return JSON.stringify(_.property(path.slice(0, -2))(lang));
+			} else if (path.endsWith('[*]')) {
+				return JSON.stringify(_.toArray(_.property(path.slice(0, -3))(lang)));
+			} else {
+				return _.property(path)(lang);
+			}
+		}
+
 		var self = this,
-			args = str.replace(/\s/g, '').slice(self.blocks.blockStart.length, self.blocks.blockEnd.length*-1).split(','),
-			lang = self.resources[self.lang],
-			template = lang[args[0]];
+			key = str.replace(/\s/g, '').slice(self.blocks.blockStart.length, self.blocks.blockEnd.length*-1),
+			template = prop(self.resources[self.lang], key);
 
 		self.count++;
 
-		if(template === undefined) {
-			self.errors.push(args[0]);
-			return args[0];
+		if (template === undefined) {
+			self.errors.push(key);
+			return str;
 		}
 
-		args.slice(1).forEach(function(arg, index){
-			template.parts[template[index]] = self.blocks.bindingStart+arg+self.blocks.bindingEnd;
-		});
-
-		return template.parts.join('');
+		return template;
 	}
 };
 
@@ -137,6 +144,6 @@ function regExpEscape(str) {
 	return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 }
 
-module.exports = function(path, opt, cb) {
+module.exports = function (path, opt, cb) {
 	return new L10n(path, opt, cb);
 };
